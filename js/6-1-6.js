7aa8e5b9dafae2e83ca3d430b935735f4c08c88e
kkenji617 <kkenji617@gmail.com>
2024-05-07 12:06:35 +0900

    レスポンシブ完成

--- js/6-1-6.js
+++ js/6-1-6.js
@@ -26,7 +26,7 @@ $(function() {
 
   responsive: [
     {
-    breakpoint: 961,//モニターの横幅が960px以下の見せ方
+    breakpoint: 1201,//モニターの横幅が960px以下の見せ方
     settings: {
       slidesToShow: 2,//スライドを画面に2枚見せる
       slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
